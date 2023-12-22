97681f816cefc2aa683c54433a6ec92d8118f5b9
Vanshdarji <134920483+Vanshdarji@users.noreply.github.com>
2023-12-22 16:42:53 +0530

    Add files via upload

---
+++ lib/main 2.dart
@@ -0,0 +1,22 @@
+import 'package:advanc_ui/Screens/home/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Food Delivery App',
+      home: HomePage(),
+    );
+  }
+}
+
